56ecbfdd612fb583a27849c77ea18ac5ad776b24
Facundo Fumaneri <facundofumaneri@Facundos-MacBook-Pro.local>
2026-09-02 21:40:39 -0300

    clase sql model

---
+++ clase-1-sqlmodel/main.py
@@ -0,0 +1,54 @@
+
+from typing import Annotated, List
+
+from fastapi import Depends, FastAPI, Query
+from sqlmodel import Field, SQLModel, Session, create_engine, select
+
+# Se define el modelo
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+
+# Code above omitted 👆
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+@app.post("/user")
+def create_user(user: User, session: SessionDep) -> User:
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
+
+@app.get("/user")
+def get_user(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> List[User]:
+    users = session.exec(select(User).offset(offset).limit(limit)).all()
+    return users
+    
+
+
