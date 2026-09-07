41040cb4aca924f1a38651be42c27360216727cb
Facundo Fumaneri <facundofumaneri@Facundos-MacBook-Pro.local>
2026-09-07 14:20:22 -0300

    arreglo relaciones

--- clase-1-sqlmodel/main.py
+++ clase-1-sqlmodel/main.py
@@ -1,16 +1,49 @@
-
 from typing import Annotated, Sequence
 
 from fastapi import Depends, FastAPI, HTTPException, Query
-from sqlmodel import Field, SQLModel, Session, create_engine, select
+from sqlmodel import (
+    Field,
+    Relationship,
+    Session,
+    SQLModel,
+    col,
+    create_engine,
+    select,
+)
 
 # Se define el modelo
 
 
-class User(SQLModel, table=True):
+class CountryBase(SQLModel):
+    name: str = Field(index=True)
+
+
+class Country(CountryBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
+    users: list["User"] = Relationship(back_populates="country")
+
+
+class CountryPublic(CountryBase):
+    id: int
+
+
+class UserBase(SQLModel):
     name: str = Field(index=True)
     age: int
+    country_id: int | None = Field(default=None, foreign_key="country.id")
+
+
+class User(UserBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    country: Country | None = Relationship(back_populates="users")
+
+
+class UserPublic(UserBase):
+    id: int
+
+
+class UserPublicWithCountry(UserPublic):
+    country: CountryPublic | None = None
 
 
 # Code above omitted 👆
@@ -26,25 +59,29 @@ def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
 
-def create_dummy_users():
+def create_dummy_data():
     with Session(engine) as session:
         if session.exec(select(User)).first():
             return
+        country_names = [("Argentina", 1), ("Brasil", 2)]
+        countries = [Country(name=name, id=id) for name, id in country_names]
+
+        session.add_all(countries)
+        session.commit()
+
         names_and_ages = [
-            ("Martina Gómez", 28),
-            ("Santiago Fernández", 34),
-            ("Valentina López", 22),
-            ("Mateo Rodríguez", 45),
-            ("Camila Martínez", 19),
-            ("Lucas Pérez", 31),
-            ("Sofía García", 27),
-            ("Nicolás Sánchez", 40),
-            ("Julieta Díaz", 24),
-            ("Tomás Romero", 37),
+            ("Martina Gómez", 28, 1),
+            ("Santiago Fernández", 34, 1),
+            ("Valentina López", 22, 1),
+            ("Mateo Rodríguez", 45, 1),
+            ("Camila Martínez", 19, 2),
+            ("Lucas Pérez", 31, 2),
+            ("Sofía García", 27, 2),
+            ("Nicolás Sánchez", 40, 2),
+            ("Julieta Díaz", 24, 2),
+            ("Tomás Romero", 37, 2),
         ]
-        users = [
-            User(name=name, age=age) for name, age in names_and_ages
-        ]
+        users = [User(name=name, age=age, country_id=country) for name, age, country in names_and_ages]
         session.add_all(users)
         session.commit()
 
@@ -62,7 +99,7 @@ app = FastAPI()
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
-    create_dummy_users()
+    create_dummy_data()
 
 
 @app.post("/user")
@@ -73,19 +110,36 @@ def create_user(user: User, session: SessionDep) -> User:
     return user
 
 
-@app.get("/user")
+@app.get("/user", response_model=Sequence[UserPublicWithCountry])
 def get_user(
     session: SessionDep,
     offset: int = 0,
     limit: Annotated[int, Query(le=100)] = 100,
 ) -> Sequence[User]:
-    users = session.exec(select(User).offset(offset).limit(limit)).all()
+    statement = select(User).offset(offset).limit(limit)
+    result = session.exec(statement)
+    users = result.all()
     return users
 
 
-@app.get("/user/{user_id}")
+@app.get("/user/{user_id}", response_model=UserPublicWithCountry)
 def get_user_by_id(user_id: int, session: SessionDep) -> User:
     user = session.get(User, user_id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
+
+
+@app.get("/user/search/{name}")
+def search_user(name: str, session: SessionDep) -> Sequence[User]:
+    statement = select(User).where(col(User.name).like("%{}%".format(name)))
+    result = session.exec(statement)
+    return result.all()
+
+
+@app.get("/user_mayores")
+def search_mayores(session: SessionDep) -> Sequence[User]:
+    # TODO: users.age >= 18
+    statement = select(User).where(User.age >= 18)
+    result = session.exec(statement)
+    return result.all()
